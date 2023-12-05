7638a4efd2448b81e2480caf90c90209af155d80
Yejun Park <yjchallenge@gmail.com>
2023-12-04 19:00:05 -0600

    put

--- frontend/src/component/update/update.js
+++ frontend/src/component/update/update.js
@@ -46,7 +46,7 @@ class Update extends React.Component {
     console.log("Sending video data:", videosData); // Log the data being sent
 
     if (fileName.toLowerCase() === 'video.txt') {
-      axios.put('http://localhost:8000/videos/${videosData.video_id}/', videosData)
+      axios.put(`http://localhost:8000/videos/${videosData.video_id}/`, videosData)
         .then(response => {
           console.log("Videos changed successfully:", response.data);
         })
@@ -54,7 +54,7 @@ class Update extends React.Component {
           console.error("Error adding videos:", error);
         });
     } else if (fileName.toLowerCase() === 'statistic.txt') {
-      axios.put('http://localhost:8000/statistics/${videosData.statistics_id}/', videosData)
+      axios.put(`http://localhost:8000/statistics/${videosData.statistics_id}/`, videosData)
         .then(response => {
           console.log("Videos changed successfully:", response.data);
         })
@@ -62,7 +62,7 @@ class Update extends React.Component {
           console.error("Error adding videos:", error);
         });
     } else if (fileName.toLowerCase() === 'youtuber.txt') {
-      axios.post('http://localhost:8000/youtubers/${videosData.channel_id}', videosData)
+      axios.put(`http://localhost:8000/youtubers/${videosData.channel_id}/`, videosData)
         .then(response => {
           console.log("Videos changed successfully:", response.data);
         })
